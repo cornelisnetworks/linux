84581c62d660979625b14b457f4d7258e3c5a48c
Dean Luick <dean.luick@cornelisnetworks.com>
2023-06-16 14:31:51 -0400

    RDMA/hfi1: Add JKR BAR0 map parameters
    Add BAR0 map support for the JKR chip.
    
    Signed-off-by: Dean Luick <dean.luick@cornelisnetworks.com>
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>

--- drivers/infiniband/hw/hfi1/chip_jkr.h
+++ drivers/infiniband/hw/hfi1/chip_jkr.h
@@ -8,4 +8,20 @@
 
 #include "chip_registers_jkr.h"
 
+/* items not defined in the generated register file */
+#define JKR_PIO_SEND (JKR_TXE + JKR_TXE_PIO_SEND_OFFSET)
+#define JKR_RCV_ARRAY (JKR_RXE + JKR_RXE_RCV_ARRAY_EGR_OFFSET)
+
+/*
+ * The JKR BAR space is not split up by the RcvArray.  To maintain
+ * compatibility with WFR, arbitrarily split the BAR space at some
+ * page-aligned spot. Use JKR_RXE - the start of the RXE block.
+ */
+#define JKR_BAR0_SIZE (128 * 1024 * 1024)	/* 128 MB */
+#define JKR_KREG1_SIZE JKR_RXE
+#define JKR_KREG2_OFFSET JKR_RXE
+#define JKR_KREG2_SIZE (JKR_PIO_SEND - JKR_RXE)
+
+#define JKR_RCV_ARRAY_SIZE (64 * 1024 * 1024)	/* 64 MB */
+
 #endif /* _CHIP_JKR_H */
