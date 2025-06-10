3863cb74452a9bc3d313b580af319f12ec573780
Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>
2025-06-10 12:39:11 -0400

    RDMA/hfi1: Remove opa_vnic
    OPA Vnic has been abandoned and left to rot. Time to excise.
    
    Signed-off-by: Dennis Dalessandro <dennis.dalessandro@cornelisnetworks.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -2250,7 +2250,6 @@ struct ib_port_data {
 
 /* rdma netdev type - specifies protocol type */
 enum rdma_netdev_t {
-	RDMA_NETDEV_OPA_VNIC,
 	RDMA_NETDEV_IPOIB,
 };
 
@@ -2264,11 +2263,6 @@ struct rdma_netdev {
 	u32		   port_num;
 	int                mtu;
 
-	/*
-	 * cleanup function must be specified.
-	 * FIXME: This is only used for OPA_VNIC and that usage should be
-	 * removed too.
-	 */
 	void (*free_rdma_netdev)(struct net_device *netdev);
 
 	/* control functions */
